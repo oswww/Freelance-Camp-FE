222eaeff680ac5b4b37d1c3171ba98d26e02efa2
Osw <osw@OswdeMacBook-Pro.local>
2017-05-13 22:35:44 +0100

    submit new proposal

--- src/app/proposals/proposal-new/proposal-new.component.ts
+++ src/app/proposals/proposal-new/proposal-new.component.ts
@@ -1,17 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs/Rx';
+
 import { Proposal } from '../proposal';
+import { ProposalService } from '../proposal.service';
 
 
 @Component({
   selector: 'app-proposal-new',
   templateUrl: './proposal-new.component.html',
-  styleUrls: ['./proposal-new.component.css']
+  styleUrls: ['./proposal-new.component.css'],
+  providers: [ ProposalService ],
 })
 export class ProposalNewComponent implements OnInit {
-  proposal = new Proposal
-  constructor() { }
+  proposal = new Proposal;
+  submitting: boolean = false;
+  submitted = false;
+  constructor(
+    private proposalService: ProposalService
+  ) {}
 
   ngOnInit() {
+  }
+
+  createProposal(proposal) {
+    this.submitting = true;
+    this.proposalService.createProposal(proposal)
+        .subscribe(
+          date => true,
+          error => {
+            console.log("Error saving proposal");
+            return Observable.throw(error);
+          }
+        );
+    this.submitted = true;
   }
 
 }
